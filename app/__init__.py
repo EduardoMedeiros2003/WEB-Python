76faf7a79c702ca8db6943ee6a2ec0f23be786da
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2026-02-23 01:38:10 -0300

    feat(products): implement get product by id endpoint
    - Set up MongoDB local connection via MONGO_URI (.env)
    - Applied configuration separation using config.py
    - Integrated PyMongo into Flask application factory
    - Implemented product listing endpoint (GET /products)
    - Implemented product detail endpoint (GET /product/<id>)
    - Handled ObjectId conversion for proper JSON responses
    - Structured API following REST principles

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
-from .roultes.main import main_bp
+from pymongo import MongoClient
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('config.Config')
+    global db
+    try:
+        client = MongoClient(app.config['MONGO_URI'])
+        db = client.get_default_database()
+    except Exception as e: 
+        print(f'erro ao realizar a conexão com o banco de dados: {e}')
+        
+    from .roultes.main import main_bp
     app.register_blueprint(main_bp)
     return app
--- app/roultes/main.py
+++ app/roultes/main.py
@@ -2,6 +2,8 @@
 from flask import Blueprint, jsonify, request
 from app.models.user import LoginPayload
 from pydantic import ValidationError
+from app import db
+from bson import ObjectId
 
 main_bp = Blueprint('main_bp', __name__)
 
@@ -17,15 +19,21 @@ def login():
         return jsonify({"error":"Erro durante a requisição do dado"}), 500
 
     if user_data.username == 'admin' and user_data.password == '123':
-        return jsonify({"message":"Login bem-sucediso!"})
+        return jsonify({"message":"Login bem-sucediso!"}),200
     else:
-        return jsonify({"message":"Credenciais invalidas"})
+        return jsonify({"message":"Credenciais invalidas"}),401
 
 
 # RF: O sistema deve permitir listagem de todos os produtos
 @main_bp.route('/products', methods=['GET'])
 def get_products():
-    return jsonify({"message":"Esta é a rota de listagem dos produtos"})
+    products_cursor = db.products.find({})
+    products_list = []
+    for product in products_cursor:
+        product['_id'] = str(product['_id'])
+        products_list.append(product)
+
+    return jsonify(products_list)
 
 # RF: O sistema deve permitir a criacao de um novo produto
 @main_bp.route('/products', methods=['POST'])
@@ -33,9 +41,20 @@ def create_product():
     return jsonify({"message":"Esta é a rota de criação de produto"})
 
 # RF: O sistema deve permitir a visualizacao dos detalhes de um unico produto
-@main_bp.route('/product/<int:product_id>', methods=['GET'])
+@main_bp.route('/product/<string:product_id>', methods=['GET'])
 def get_product_by_id(product_id):
-    return jsonify({"message":f"Esta é a rota de visualizacao do detalhe do id do produto {product_id}"})
+    try: 
+        oid = ObjectId(product_id)
+    except Exception as e:
+        return jsonify({"error": f"Erro ao transformar o {product_id} em ObjectID: {e}"}),400
+    
+    product = db.products.find_one({"_id": oid})
+
+    if product:
+        product['_id'] = str(product['_id'])
+        return jsonify(product),200
+    else:
+        return jsonify({"erro": f"Produto com o id: {product_id} - Não encontrado"}),404
 
 # RF: O sistema deve permitir a atualizacao de um unico produto e produto existente
 @main_bp.route('/product/<int:product_id>', methods=['PUT'])
@@ -55,5 +74,4 @@ def upload_sales():
 @main_bp.route('/')
 def index():
     return jsonify({'message':'Bem vindo ao Stylesync!'})
-
 
